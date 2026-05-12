14275170c79790114741a2c665fafdfa625b08ee
Ilnur Shaykhutdinov <ilnur888@gmail.com>
2026-05-12 12:30:10 +0000

    First build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
-    id("org.jetbrains.intellij.platform") version "2.6.0"
+    kotlin("jvm") version "2.3.0"
+    id("org.jetbrains.intellij.platform") version "2.16.0"
 }
 
 group = "com.ilnur.calculator"
