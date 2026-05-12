af61a3dc22d41c5337945a3c5faf5c94f2b7c57f
Ilnur Shaykhutdinov <ilnur888@gmail.com>
2026-05-12 12:44:33 +0000

    Format with dot instead of locale

--- src/main/kotlin/com/ilnur/calculator/ExpressionEvaluator.kt
+++ src/main/kotlin/com/ilnur/calculator/ExpressionEvaluator.kt
@@ -2,10 +2,12 @@ package com.ilnur.calculator
 
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.Locale
 
 object ExpressionEvaluator {
 
-    private val formatter = DecimalFormat("0.00")
+    private val formatter = DecimalFormat("0.00", DecimalFormatSymbols(Locale.US))
 
     fun evaluate(expression: String): String {
 
